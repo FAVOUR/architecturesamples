f6bc97972c9044bba4f87129137180bfde24e841
FAVOUR <favouro@tractionapps.co>
2024-11-01 22:40:43 +0100

    Setup sonarCloud with Github

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,21 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+//plugins {
+//    alias(libs.plugins.androidApplication) apply false
+//    alias(libs.plugins.jetbrainsKotlinAndroid) apply false
+//
+//}
+
+
 plugins {
+    id("org.sonarqube") version "5.1.0.4882"
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "FAVOUR_architecturesamples")
+        property("sonar.organization", "favour")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
 }
